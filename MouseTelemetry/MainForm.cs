e98b1d5cbb8a310fd44633be51800f5755aae4e6
Juan Manuel Servera <jmservera@outlook.com>
2014-12-12 13:23:20 +0100

    add event producer

---
+++ MouseTelemetry/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using MouseKeyboardActivityMonitor;
+using MouseKeyboardActivityMonitor.WinApi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseTelemetry
+{
+    public partial class MainForm : Form
+    {
+        MouseHookListener _mouseListener;
+        Sender _sender;
+
+        public MainForm()
+        {
+            //GlobalProxySelection.Select = new WebProxy("127.0.0.1", 8888);
+
+            InitializeComponent();
+            _mouseListener = new MouseHookListener(new GlobalHooker());
+            _mouseListener.MouseMove += listener_MouseMove;
+
+            _sender = new Sender("telemetry");
+        }
+
+        void listener_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            label1.Text = e.X.ToString();
+            label2.Text = e.Y.ToString();
+            _sender.SendAsync(e.X, e.Y);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _mouseListener.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _mouseListener.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var pos=System.Windows.Forms.Cursor.Position;
+            _sender.SendAsync(pos.X, pos.Y);
+        }
+    }
+}
---
+++ Netsaimada.IoT.CloudService/Netsaimada.IoT.CloudService.Receiver/EventProcessor.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netsaimada.IoT.CloudService.Receiver
+{
+    public class EventProcessor:IEventProcessor
+    {
+        PartitionContext partitionContext;
+        Stopwatch checkpointStopWatch;
+
+        CloudTableClient _tableClient;
+
+        public async Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Console.WriteLine(string.Format("Processor Shuting Down.  Partition '{0}', Reason: '{1}'.", this.partitionContext.Lease.PartitionId, reason.ToString()));
+            if (reason == CloseReason.Shutdown)
+            {
+                await context.CheckpointAsync();
+            }
+        }
+
+        public Task OpenAsync(PartitionContext context)
+        {
+            Console.WriteLine(string.Format("SimpleEventProcessor initialize.  Partition: '{0}', Offset: '{1}'", context.Lease.PartitionId, context.Lease.Offset));
+            this.partitionContext = context;
+            this.checkpointStopWatch = new Stopwatch();
+            this.checkpointStopWatch.Start();
+            return Task.FromResult<object>(null);
+        }
+
+        public async Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            try
+             {
+                 foreach (EventData eventData in messages)
+                 {
+                     Console.WriteLine("Processing event hub data...");
+ 
+                     string key = eventData.PartitionKey;
+ 
+                     string data = System.Text.Encoding.Unicode.GetString(eventData.GetBytes());
+                     try
+                     {
+                         var json = JObject.Parse(data);
+                         //string text = json["Message"].ToString();
+                         //string agent = json["BrowserInfo"].ToString();
+ 
+                         //if (queue != null)
+                         //{
+                             
+                         //    await queue.SendAsync(new BrokeredMessage((agent + "##" + text)));
+ 
+                         //    Trace.TraceInformation("Added to queue: " + agent);
+                         //}
+                     }
+                     catch(Exception exx)
+                     {
+                         Console.WriteLine(exx.Message);
+                     }
+ 
+                     Console.WriteLine(string.Format("Message received.  Partition: '{0}', Device: '{1}'",
+                         this.partitionContext.Lease.PartitionId, key));
+                 }
+ 
+                 //Call checkpoint every 5 minutes, so that worker can resume processing from the 1 minutes back if it restarts.
+                 // Doing every ONE MINUTE now.
+                 if (this.checkpointStopWatch.Elapsed > TimeSpan.FromMinutes(1))
+                 {
+                     await context.CheckpointAsync();
+                     lock (this)
+                     {
+                         this.checkpointStopWatch.Reset();
+                     }
+                 }
+             }
+             catch (Exception exp)
+             {
+                 Console.WriteLine("Error in processing: " + exp.Message);
+             }
+        }
+    }
+}

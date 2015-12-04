4fb57f16be8ca8aef09419640d7326685606084a
Juan Manuel Servera <jmservera@outlook.com>
2015-12-04 20:51:09 +0100

    Use ConnectTheDots website

--- IoTSensors/DhtView/MainPage.xaml.cs
+++ IoTSensors/DhtView/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using Microsoft.Azure.Devices.Client;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -26,16 +27,29 @@ namespace DhtView
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        const string iotHubUri = "jmservera.azure-devices.net";
+        const string rpiName = "Raspi001";
+        const string deviceKey = "UqVO9AzslYU8R8A7wek3pn7DT1gnCbuzI611umSasxc=";
+        const string GUID = "58980B74-8117-464A-A9FA-1850B0E2F0B3"; //todo create a unique guid per device
+        const string ORGANIZATION = "Microsoft";
+        const string DISPLAYNAME = "Raspberry Pi 2 DHT22";
+        const string LOCATION = "Madrid"; //todo config the location
+        const string TEMPMEASURE = "Temperature";
+        const string HUMIDMEASURE = "Humidity";
+        const string TEMPUNITS = "C";
+        const string HUMIDUNITS = "%";
+        const string jsonFormat = "{{\"guid\":\"{0}\", \"organization\":\"{1}\", \"displayname\": \"{2}\", \"location\": \"{3}\", \"measurename\": \"{4}\", \"unitofmeasure\": \"{5}\", \"value\":{6}, \"timecreated\":\"{7}\" }}";
+
+        const int pinNumber = 4;
+
+        static SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
+        static SolidColorBrush orangeBrush = new SolidColorBrush(Windows.UI.Colors.Orange);
+
         public MainPage()
         {
             this.InitializeComponent();
             InitializeSensor();
         }
-        private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
-        private SolidColorBrush orangeBrush = new SolidColorBrush(Windows.UI.Colors.Orange);
-        const string iotHubUri = "sqlsathub.azure-devices.net";
-        const string deviceKey = "WznTpvxyTJcme0dSUYdGNH/KneVXxztbnvlcAuFEKFc=";
-        const string rpiName = "sqlsat_01";
 
         private async void InitializeSensor()
         {
@@ -58,12 +72,11 @@ namespace DhtView
             {
                 try
                 {
-                    receivedMessage = await deviceClient.ReceiveAsync();// TimeSpan.FromSeconds(1));
-
+                    receivedMessage = await deviceClient.ReceiveAsync();
                     if (receivedMessage != null)
                     {
                         messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
-                        System.Diagnostics.Debug.WriteLine(String.Format("\t{0}> Received message: {1}", DateTime.Now.ToLocalTime(), messageData));
+                        Debug.WriteLine(String.Format("\t{0}> Received message: {1}", DateTime.Now.ToLocalTime(), messageData));
                         messages.Text = $"{DateTime.Now.ToLocalTime()}> Received message: {messageData}";
                         await deviceClient.CompleteAsync(receivedMessage);
                     }
@@ -71,7 +84,7 @@ namespace DhtView
                 }
                 catch (Exception ex)
                 {
-                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                    Debug.WriteLine(ex.Message);
                     await Task.Delay(recoverTimeout);
                     recoverTimeout *= 10; // increment timeout for connection recovery
                     if (recoverTimeout > 600000)//set a maximum timeout
@@ -83,24 +96,11 @@ namespace DhtView
 
         }
 
-        const string GUID = "58980B74-8117-464A-A9FA-1850B0E2F0B3"; //todo create a unique guid per device
-        const string ORGANIZATION = "Microsoft";
-        const string DISPLAYNAME = "Raspberry Pi 2 DHT22";
-        const string LOCATION = "Madrid"; //todo config the location
-        const string TEMPMEASURE = "Temperature";
-        const string HUMIDMEASURE = "Humidity";
-        const string TEMPUNITS = "C";
-        const string HUMIDUNITS = "%";
-        const string jsonFormat = "{{\"guid\":\"{0}\", \"organization\":\"{1}\", \"displayname\": \"{2}\", \"location\": \"{3}\", \"measurename\": \"{4}\", \"unitofmeasure\": \"{5}\", \"value\":{6}, \"timecreated\":\"{7:u}\" }}";
-
-
         private async Task SendEvents(DeviceClient deviceClient)
         {
             try
             {
-                string dataBuffer;
-                SensorReader c = new SensorReader(4);
-                Guid id = Guid.NewGuid();
+                SensorReader c = new SensorReader(pinNumber);
                 while (true)
                 {
                     var data = c.Read();
@@ -108,65 +108,71 @@ namespace DhtView
                     {
                         try
                         {
-                            System.Diagnostics.Debug.WriteLine(string.Format("Temp:{0} Hum:{1}", data.Temperature, data.Humidity));
                             refreshData(data);
-                            dataBuffer = string.Format(jsonFormat,
-                                GUID, ORGANIZATION, DISPLAYNAME, LOCATION, TEMPMEASURE, TEMPUNITS, data.Temperature, DateTime.UtcNow);
-                            Message eventMessage = new Message(Encoding.UTF8.GetBytes(dataBuffer));
-                            await deviceClient.SendEventAsync(eventMessage);
-                            dataBuffer = string.Format(jsonFormat,
-                                GUID, ORGANIZATION, DISPLAYNAME, LOCATION, HUMIDMEASURE, HUMIDUNITS, data.Humidity, DateTime.UtcNow);
-                            eventMessage = new Message(Encoding.UTF8.GetBytes(dataBuffer));
-                            await deviceClient.SendEventAsync(eventMessage);
+
+                            await sendData(deviceClient, data);
                         }
                         catch (Exception ex)
                         {
-                            System.Diagnostics.Debug.WriteLine(ex.Message);
+                            Debug.WriteLine(ex.Message);
                         }
                     }
+                    //sensor should be read every 2 seconds
                     await Task.Delay(2000).ConfigureAwait(false);
                 }
             }
             catch (Exception ex)
             {
-                System.Diagnostics.Debug.WriteLine(string.Format("{0}: {1}", ex.Message, ex.StackTrace));
+                Debug.WriteLine(string.Format("{0}: {1}", ex.Message, ex.StackTrace));
             }
+        }
+
+        private static async Task<string> sendData(DeviceClient deviceClient, Dht11Reading data)
+        {
+            var timeCreated = $"{DateTime.UtcNow:u}".Replace(' ', 'T');
+            Debug.WriteLine($"Temp:{data.Temperature} Hum:{data.Humidity} Time:{timeCreated}");
+
+            string dataBuffer = string.Format(jsonFormat,
+                GUID, ORGANIZATION, DISPLAYNAME, LOCATION, TEMPMEASURE, TEMPUNITS, data.Temperature, timeCreated);
+            Message eventMessage = new Message(Encoding.UTF8.GetBytes(dataBuffer));
+            await deviceClient.SendEventAsync(eventMessage);
+            dataBuffer = string.Format(jsonFormat,
+                GUID, ORGANIZATION, DISPLAYNAME, LOCATION, HUMIDMEASURE, HUMIDUNITS, data.Humidity, timeCreated);
+            eventMessage = new Message(Encoding.UTF8.GetBytes(dataBuffer));
+            await deviceClient.SendEventAsync(eventMessage);
+            return dataBuffer;
         }
 
         private async void refreshData(Dht11Reading data)
         {
+            if (data.IsValid)
             {
                 await textBlockTemp.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-                {
-                    if (data.IsValid)
-                    {
-                        this.textBlockTemp.Text = data.Temperature + "°C";
-                        this.textBlockHum.Text = data.Humidity.ToString() + "%";
-                        this.reading.Text = "";
-                        if (data.Temperature >= 30)
                         {
-                            rectangleTempDown.Fill = redBrush;
-                        }
-                        else
-                        {
-                            rectangleTempDown.Fill = orangeBrush;
-                        }
-                        if (data.Humidity >= 50)
-                        {
-                            rectangleHumDown.Fill = redBrush;
-                        }
-                        else
-                        {
-                            rectangleHumDown.Fill = orangeBrush;
-                        }
-                        rectangleTempUp.Height = 2 * (100 - data.Temperature);
-                        rectangleTempDown.Height = 2 * data.Temperature;
-                        rectangleHumUp.Height = 2 * (100 - data.Humidity);
-                        rectangleHumDown.Height = 2 * data.Humidity;
-
-                    }
-                });
-
+                            this.textBlockTemp.Text = data.Temperature + "°C";
+                            this.textBlockHum.Text = data.Humidity.ToString() + "%";
+                            this.reading.Text = "";
+                            if (data.Temperature >= 30)
+                            {
+                                rectangleTempDown.Fill = redBrush;
+                            }
+                            else
+                            {
+                                rectangleTempDown.Fill = orangeBrush;
+                            }
+                            if (data.Humidity >= 50)
+                            {
+                                rectangleHumDown.Fill = redBrush;
+                            }
+                            else
+                            {
+                                rectangleHumDown.Fill = orangeBrush;
+                            }
+                            rectangleTempUp.Height = 2 * (100 - data.Temperature);
+                            rectangleTempDown.Height = 2 * data.Temperature;
+                            rectangleHumUp.Height = 2 * (100 - data.Humidity);
+                            rectangleHumDown.Height = 2 * data.Humidity;
+                        });
             }
         }
     }

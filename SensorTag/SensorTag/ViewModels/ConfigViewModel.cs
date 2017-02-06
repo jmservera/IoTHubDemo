244fe0eb39f1cd64c3ad70981502377d0f52d988
juanserv <juanserv@DESKTOP-K6ICE1B>
2017-02-06 19:22:33 +0100

    add minimal config

---
+++ SensorTag/SensorTag/ViewModels/ConfigViewModel.cs
@@ -0,0 +1,21 @@
+﻿using SensorTag.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+
+namespace SensorTag.ViewModels
+{
+
+    public class ConfigViewModel : ViewModelBase
+    {
+        private Config config = Config.Default;
+        public Config CurrentConfig
+        {
+            get { return config; }
+            set { Set(ref config, value); }
+        }
+    }
+}
